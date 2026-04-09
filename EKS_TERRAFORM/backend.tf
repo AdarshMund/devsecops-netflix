4db815a4c9c4a21ed027c62eff75c1565ca1279a
AdarshMund <adarshkumarmund755@gmail.com>
2026-04-10 00:11:50 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-netflix-reyaz-16" # Replace with your actual S3 bucket name
+    bucket = "devsecops-netflix-adarsh-1123" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
